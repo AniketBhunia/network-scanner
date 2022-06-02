162c827cf166840a172c650d44280747fd1a6c9b
Berlin840 <bhuniaaniket70@gmail.com>
2022-06-02 23:23:31 +0530

    Update NetworkScanner.py

--- NetworkScanner.py
+++ NetworkScanner.py
@@ -1,27 +1,39 @@
-from scapy.all import ARP, Ether, srp
+import scapy.all as scapy 
+import argparse
 
-target_ip = "192.168.0.0/24"   # IP Address for the destination.
-                            
-arp = ARP(pdst=target_ip)      # Creating ARP packets.
- 
-    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", dest="target", help="Target IP / IP Range")
+    options = parser.parse_args()
+    if not options.target:
+        parser.error("[!] Please add an interface to proceed (like : 192.168.1.1/24), --help for more informations.")
+    return options
 
-    packet = ether/arp
-
-result = srp(packet, timeout=3, verbose=0)[0]
-
-clients = []
-
-for sent, received in result:
-    
-    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+def scan_network(target_ip):                            
+   arp_request = scapy.ARP(pdst=target_ip)      # Creating ARP packets.
+   broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+   packet = broadcast/arp_request 
+   ask_list = scapy.srp(packet, timeout = 3, verbose = False)[0]
+   
+   packet_list = []
+   for i in ask_list:
+        packet_dict = {"ip" : i[1].psrc, "mac" : i[1].hwsrc}
+        packet_list.append(packet_dict)
+        return(packet_list)
 
 print("Available devices in the network:")
-
 print("IP" + " "*18+"MAC")
 
-for client in clients:
-  
-print("{:16}    {}".format(client['ip'], client['mac']))
+def print_result(res):
+    print(""" __  _ ___ _____ _   _  __  ___ _  __    __   ___ __  __  _ __  _ ___ ___   
+|  \| | __|_   _| | | |/__\| _ \ |/ /  /' _/ / _//  \|  \| |  \| | __| _ \  
+| | ' | _|  | | | 'V' | \/ | v /   <   `._`.| \_| /\ | | ' | | ' | _|| v /  
+|_|\__|___| |_| !_/ \_!\__/|_|_\_|\_\  |___/ \__/_||_|_|\__|_|\__|___|_|_\  """)
+    print("=========================================")
+    print("IP\t\t\tMAC Address\n=========================================")
+    for n in res:
+        print(n["ip"] + "\t\t" + n["mac"])
 
-{"mode":"full","isActive":"false"}
+options = get_arguments()
+scan_result = scan_network(options.target)
+print_result(scan_result)
